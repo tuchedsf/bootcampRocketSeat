86dbec63b7b32c16567a61229da921791c66c6b1
p060651 <diego.ferreira@prodemge.gov.br>
2019-06-25 14:45:07 -0300

    configuracao inicial mvc, ate criacao dos models

--- nodejsModulo2/src/server.js
+++ nodejsModulo2/src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 class App {
   constructor () {
     this.express = express()
-    this.isDev = process.env.NODE_ENV != 'production'
+    this.isDev = process.env.NODE_ENV !== 'production'
 
     this.middlewares()
     this.views()
@@ -23,6 +23,10 @@ class App {
       express: this.express,
       watch: this.isDev
     })
+    // para o express passar a exibir o diretorio public e ler os arquivos la
+    this.express.use(express.static(path.resolve(__dirname, 'public')))
+
+    // setando o view engine
     this.express.set('view engine', 'njk')
   }
 
