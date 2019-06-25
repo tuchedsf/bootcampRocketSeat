ba57fcac8505696fa07d885ffd04132f68961e17
p060651 <diego.ferreira@prodemge.gov.br>
2019-06-25 16:42:45 -0300

    rota store

--- nodejsModulo2/src/app/controllers/UserController.js
+++ nodejsModulo2/src/app/controllers/UserController.js
@@ -1,6 +1,12 @@
+const { User } = require('../models')
 class UserController {
   create (req, res) {
     return res.render('auth/signup')
+  }
+
+  async store (req, res) {
+    await User.create(req.body)
+    return res.redirect('/')
   }
 }
 
--- nodejsModulo2/src/routes.js
+++ nodejsModulo2/src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 
 routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 routes.get('/', (req, res) => {
   return res.render('auth/signup')
