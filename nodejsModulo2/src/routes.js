534aedc907053b621d28516701e3e6f670e56204
p060651 <diego.ferreira@prodemge.gov.br>
2019-06-25 14:55:21 -0300

    criacao de userController e adequacao das rotas para usar as rotas do userController.

--- nodejsModulo2/src/routes.js
+++ nodejsModulo2/src/routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const routes = express.Router()
 
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/signup', UserController.create)
+
 routes.get('/', (req, res) => {
   return res.render('auth/signup')
 })
