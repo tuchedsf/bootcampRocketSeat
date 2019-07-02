c913dce5504985a8815bd147b4e3978311390d6e
p060651 <diego.ferreira@prodemge.gov.br>
2019-07-02 16:43:17 -0300

    ajuste arquvos

--- goBarber/src/app/dicas.js
+++ goBarber/src/app/dicas.js
@@ -1,8 +1,28 @@
 /**
+ * ###VSCODE###
+ * Instalar plugin eslint
+ * instalar plugin prettier
+ *
+ * Setings:
+ * eslint.autoFixOnSave = true
+ * editor.formatOnSave = false
+ *
+ *
+ *
  * #### ESLINT ######
  * yarn eslint --init
  * Comando para iniciar o eslint apos fazer a instalação. Ira fazer as configurações basicas.
  * como estamos usando o yarn pode-se deletar o arquivo package.lock em seguida rodar o comando:
  * yarn ( ira recarregar as dependencias que estavam no package.lock para o yarn.lock)
+ *
+ *
+ * ### SEQUELIZE ###
+ * yarn add sequelize
+ * yarn add sequelize-cli
+ *
+ *
+ * Aula
+ *
+ * parei sequelize 4:54
  *
  *  */
