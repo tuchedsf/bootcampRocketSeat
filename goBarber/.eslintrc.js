080d1a349432641b0fc579f2ed0adfc42157d13f
Diego Ferreira <diego.diegoferreira@gmail.com>
2019-07-02 23:26:17 -0300

    config eslint / prettier / editorConfig

--- goBarber/.eslintrc.js
+++ goBarber/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["airbnb-base"],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
   globals: {
     Atomics: "readable",
     SharedArrayBuffer: "readable"
@@ -12,5 +13,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  rules: {}
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off", //com esse parametro nao é necessario usar o this dentro dos metodos das classes
+    "no-param-reassign": "off", // permite manipular o valor do parametro de um metodo
+    camelcase: "off", //permitir variavel com "_" ex.: numero_endereco senao eslint apenas aceita variavel numeroEndereco
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }] //exibe msg erro variaveis nao usadas mais ignora o next
+  }
 };
