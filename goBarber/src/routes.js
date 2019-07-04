b306a1af95ba5853851fcb7ddcc4d6baa7658c55
Diego Ferreira <diego.diegoferreira@gmail.com>
2019-07-03 21:26:57 -0300

    configuracao model

--- goBarber/src/routes.js
+++ goBarber/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from '../src/app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego',
+    email: 'teste@test.com',
+    password_hash: 'teste',
+    avatar: 'teste.jpg',
+  });
+  return res.json(user);
 });
 
 export default routes;
